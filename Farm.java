0c0d4899fdbdebda76c88c6d794f2ed709ef3246
Boose101 <51867298+Boose101@users.noreply.github.com>
2022-09-20 10:26:56 -0400

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -4,17 +4,17 @@ public class Farm {
     public ArrayList<String> names = new ArrayList<String>();
     public void generator(){
         
-        private static void ReadFile() {
-        try{
-            File myObj = new File("names.txt");
-            Scanner reader = new Scanner(myObj);
-            while(reader.hasNextLine()){
-                names.add(reader.nextLine());
+        private static void ReadFile(){
+            try{
+                File myObj = new File("names.txt");
+                Scanner reader = new Scanner(myObj);
+                while(reader.hasNextLine()){
+                    names.add(reader.nextLine());
+                }
+                reader.close();
+            }catch (FileNotFoundException fnf){
+                fnf.printStackTrace();
             }
-            reader.close();
-        }catch (FileNotFoundException fnf){
-            fnf.printStackTrace();
         }
-    }
     }
 }
